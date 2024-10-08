b5ad92d311a9823e8da68ae6820bfbd9ec667a64
kwak-jiho <113403560+kwak-jiho@users.noreply.github.com>
2024-10-08 21:14:44 +0900

    init

---
+++ src/main.js
@@ -0,0 +1,407 @@
+/**
+* Template Name: Arsha
+* Template URL: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/
+* Updated: Mar 17 2024 with Bootstrap v5.3.3
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+
+(function () {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
+   * Navbar links active state on scroll
+   */
+  let navbarlinks = select('#navbar .scrollto', true)
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+      if (!navbarlink.hash) return
+      let section = select(navbarlink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active')
+      } else {
+        navbarlink.classList.remove('active')
+      }
+    })
+  }
+  window.addEventListener('load', navbarlinksActive)
+  onscroll(document, navbarlinksActive)
+
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let header = select('#header')
+    let offset = header.offsetHeight
+
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: 'smooth'
+    })
+  }
+
+  /**
+   * Toggle .header-scrolled class to #header when page is scrolled
+   */
+  let selectHeader = select('#header')
+  if (selectHeader) {
+    const headerScrolled = () => {
+      if (window.scrollY > 100) {
+        selectHeader.classList.add('header-scrolled')
+      } else {
+        selectHeader.classList.remove('header-scrolled')
+      }
+    }
+    window.addEventListener('load', headerScrolled)
+    onscroll(document, headerScrolled)
+  }
+
+  /**
+   * Back to top button
+   */
+  let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
+
+  /**
+   * Mobile nav toggle
+   */
+  on('click', '.mobile-nav-toggle', function (e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
+
+  /**
+   * Mobile nav dropdowns activate
+   */
+  on('click', '.navbar .dropdown > a', function (e) {
+    if (select('#navbar').classList.contains('navbar-mobile')) {
+      e.preventDefault()
+      this.nextElementSibling.classList.toggle('dropdown-active')
+    }
+  }, true)
+
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  on('click', '.scrollto', function (e) {
+    if (select(this.hash)) {
+      e.preventDefault()
+
+      let navbar = select('#navbar')
+      if (navbar.classList.contains('navbar-mobile')) {
+        navbar.classList.remove('navbar-mobile')
+        let navbarToggle = select('.mobile-nav-toggle')
+        navbarToggle.classList.toggle('bi-list')
+        navbarToggle.classList.toggle('bi-x')
+      }
+      scrollto(this.hash)
+    }
+  }, true)
+
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
+  window.addEventListener('load', () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash)
+      }
+    }
+  });
+
+  /**
+   * Preloader
+   */
+  let preloader = select('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove()
+    });
+  }
+
+  /**
+   * Initiate  glightbox 
+   */
+  const glightbox = GLightbox({
+    selector: '.glightbox'
+  });
+
+  /**
+   * Skills animation
+   */
+  let skilsContent = select('.skills-content');
+  if (skilsContent) {
+    new Waypoint({
+      element: skilsContent,
+      offset: '80%',
+      handler: function (direction) {
+        let progress = select('.progress .progress-bar', true);
+        progress.forEach((el) => {
+          el.style.width = el.getAttribute('aria-valuenow') + '%'
+        });
+      }
+    })
+  }
+
+  /**
+   * Porfolio isotope and filter
+   */
+  window.addEventListener('load', () => {
+    let portfolioContainer = select('.portfolio-container');
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: '.portfolio-item'
+      });
+
+      let portfolioFilters = select('#portfolio-flters li', true);
+
+      on('click', '#portfolio-flters li', function (e) {
+        e.preventDefault();
+        portfolioFilters.forEach(function (el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
+
+        portfolioIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+        portfolioIsotope.on('arrangeComplete', function () {
+          AOS.refresh()
+        });
+      }, true);
+    }
+
+  });
+
+  /**
+   * Initiate portfolio lightbox 
+   */
+  const portfolioLightbox = GLightbox({
+    selector: '.portfolio-lightbox'
+  });
+
+  /**
+   * Portfolio details slider
+   */
+  new Swiper('.portfolio-details-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false
+    });
+  });
+
+})()
+
+const data = [
+  { status: '마감', department: '한국산업기술기획평가원', title: '파운데이션 라이브러리 PPA 성능 향상 기술 개발', startDate: '2023-03-10', endDate: '2023-03-20', link: 'https://www.iris.go.kr/contents/retrieveBsnsAncmView.do?ancmId=002004' },
+  { status: '마감', department: '한국산업기술기획평가원', title: '의료용 단말 기기의 양방향 고속 네트워킹 SoC', startDate: '2023-03-10', endDate: '2023-03-20', link:'https://www.iris.go.kr/contents/retrieveBsnsAncmView.do?ancmId=002004' },
+  { status: '마감', department: '한국산업기술기획평가원', title: '공정데이터기반반도체장비지능화통합패키지기술개발', startDate: '2023-03-10', endDate: '2023-03-20', link:'https://www.iris.go.kr/contents/retrieveBsnsAncmView.do?ancmId=002004' },
+  { status: '마감', department: '한국산업기술기획평가원', title: '이상 감지 및 성능 향상을 위한 지능형 원자층 증착 공정장비 기술개발', startDate: '2023-03-10', endDate: '2023-03-20', link:'https://www.iris.go.kr/contents/retrieveBsnsAncmView.do?ancmId=002004' },
+  { status: '마감', department: '한국산업기술기획평가원', title: '고선택비 하드마스크용 지능형 물리기상증착 공정장비 기술개발', startDate: '2023-03-10', endDate: '2023-03-20', link:'https://www.iris.go.kr/contents/retrieveBsnsAncmView.do?ancmId=002004' },
+  { status: '마감', department: '한국산업기술기획평가원', title: '반도체 장비 지능화를 위한 공정 데이터 플랫폼 기술 개발', startDate: '2023-03-10', endDate: '2023-03-20', link:'https://www.iris.go.kr/contents/retrieveBsnsAncmView.do?ancmId=002004' },
+  { status: '마감', department: '한국산업기술기획평가원', title: '소변내 생체표지자에 기반한 성인병 조기진단용 광학식 센서 시스템 개발', startDate: '2023-03-10', endDate: '2023-03-20', link:'https://www.iris.go.kr/contents/retrieveBsnsAncmView.do?ancmId=002004' },
+  { status: '마감', department: '한국산업기술기획평가원', title: 'Diffusion 공정 Batch type 설비 (Multi-wafer) 적용 차세대 노즐 부품 개발', startDate: '2023-03-10', endDate: '2023-03-20', link:'https://www.iris.go.kr/contents/retrieveBsnsAncmView.do?ancmId=002004' },
+  { status: '마감', department: '한국산업기술기획평가원', title: '초미세 패터닝에 최적화된 Dry resist와 Underlayer의 조합과 EUV 노광특성 평가기술 개발', startDate: '2023-03-10', endDate: '2023-03-20', link:'https://www.iris.go.kr/contents/retrieveBsnsAncmView.do?ancmId=002004' },
+  { status: '마감', department: '한국산업기술기획평가원', title: '저온경화형 Photo acid generator(PAG) 및 이를이용하는 EUV용 photoresist 개발 ', startDate: '2023-03-10', endDate: '2023-03-20', link:'https://www.iris.go.kr/contents/retrieveBsnsAncmView.do?ancmId=002004' },
+  { status: '마감', department: '한국산업기술기획평가원', title: '파운데이션 라이브러리 PPA 성능 향상 기술 개발', startDate: '2023-03-10', endDate: '2023-03-20', link:'https://www.iris.go.kr/contents/retrieveBsnsAncmView.do?ancmId=002004' },
+
+
+  // 추가 데이터는 필요에 따라 이곳에 추가
+];
+
+
+let currentPage = 1;
+const rowsPerPage = 10;
+
+function search() {
+  let query = document.getElementById('search-input').value;
+  alert('검색어: ' + query);
+  // 검색어를 백엔드로 전송
+  fetch('http://localhost:3000/search', { // URL 수정
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ query: query }),
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
+
+function toggleStatus(element) {
+  let buttons = document.getElementsByClassName('status-button');
+  for (let button of buttons) {
+    button.classList.remove('active');
+  }
+  element.classList.add('active');
+  // 클릭된 버튼의 ID를 백엔드로 전송
+  fetch('http://localhost:3000/status', { // URL 수정
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ id: element.id }),
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
+
+function toggleCategory(element) {
+  element.classList.toggle('active');
+
+  const categoryId = element.id;
+  console.log('Sending category ID:', categoryId);
+
+  fetch('http://localhost:3000/category', { // URL 수정
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ id: categoryId }),
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
+
+
+
+
+
+
+function displayList(items, wrapper, rowsPerPage, page) {
+  wrapper.innerHTML = "";
+  page--;
+
+  let start = rowsPerPage * page;
+  let end = start + rowsPerPage;
+  let paginatedItems = items.slice(start, end);
+
+  for (let i = 0; i < paginatedItems.length; i++) {
+    let item = paginatedItems[i];
+
+    let row = document.createElement('tr');
+    row.innerHTML = `
+                  <td>${item.status}</td>
+                  <td>${item.department}</td>
+                  <td>${item.title}</td>              
+                  <td>${item.startDate}</td>
+                  <td>${item.endDate}</td>
+                  <td><a href="${item.link}" target="_blank">${item.link}</a></td>
+              `;
+    wrapper.appendChild(row);
+  }
+}
+
+function setupPagination(items, wrapper, rowsPerPage) {
+  wrapper.innerHTML = "";
+
+  let pageCount = Math.ceil(items.length / rowsPerPage);
+  for (let i = 1; i < pageCount + 1; i++) {
+    let btn = paginationButton(i, items);
+    wrapper.appendChild(btn);
+  }
+}
+
+function paginationButton(page, items) {
+  let button = document.createElement('button');
+  button.innerText = page;
+
+  if (currentPage == page) button.classList.add('active');
+
+  button.addEventListener('click', function () {
+    currentPage = page;
+    displayList(items, document.getElementById('results-body'), rowsPerPage, currentPage);
+
+    let currentBtn = document.querySelector('.pagination button.active');
+    currentBtn.classList.remove('active');
+
+    button.classList.add('active');
+  });
+
+  return button;
+}
+
+displayList(data, document.getElementById('results-body'), rowsPerPage, currentPage);
+setupPagination(data, document.getElementById('pagination'), rowsPerPage);
+
